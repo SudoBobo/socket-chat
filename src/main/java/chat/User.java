bc1df2deacd8cc6601af747075985e05fb4b3a2e
SudoBobo <ivan.koptelov@phystech.edu>
2017-06-13 23:09:23 +0300

    add UUID as string id generator

--- src/main/java/chat/User.java
+++ src/main/java/chat/User.java
@@ -1,10 +1,19 @@
 package chat;
 
-import java.io.Serializable;
+import java.util.UUID;
 
-public class User implements Serializable {
-   public String name;
-   public String pass;
+public class User {
+
+    private String id = createID();
+    private String name;
+    private String pass;
+
+    private static long idCounter = 0;
+
+    private static synchronized String createID()
+    {
+        return UUID.randomUUID().toString();
+    }
 
     public User(String name, String pass) {
         this.name = name;
